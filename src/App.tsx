e3531a57b5c35ae18c837ae175c3d76d8722fd9b
Hans van Os <com.devex@outlook.com>
2023-06-18 21:14:55 +0200

    Making progress to MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,99 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.scss";
+import *  as threadService from "./ThreadService";
+import {Message, Thread} from "./ThreadService";
+import {BrowserRouter, Link, NavLink, Route, Router, Routes} from "react-router-dom";
 
 function App() {
     return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/:threadId" element={<MessageDetails/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+function Home() {
+    return (
         <div className="main-container">
-            {/*<MainView/>*/}
-            <DetailView/>
+            <MainView/>
         </div>
-    );
-}
-
-function DetailView() {
-    return (
-        <div className="split-view">
-            <div className="detail-view">
-                <Message username="Hans van Os" message="Yo!" time="22:51"></Message>
-                <Message username="Youssef Airoude" message="Waddup?" time="22:51"></Message>
-                <Message username="Mathijs Janz" message="Ewa" time="22:51"></Message>
-                <Message username="Hans van Os" message="Klaar voor de ps5 gaming?" time="22:51"></Message>
-            </div>
-            <div className="list-view">
-                <SubjectListCard username="Hans van Os" subtext="hello!" time="22:51"></SubjectListCard>
-                <SubjectListCard username="Hans van Os" subtext="hello!" time="22:51"></SubjectListCard>
-                <SubjectListCard username="Hans van Os" subtext="hello!" time="22:51"></SubjectListCard>
-            </div>
-        </div>
-    );
-}
-
-export function SubjectListCard(props: any) {
-    const subject: string = props.username;
-    const usernames: string[] = props.subtext;
-    const time = props.time;
-    return (
-        <>
-            <div className="h-card">
-                <div className="flexcolumn center">
-                    <p>{subject}</p>
-                    <small>{usernames}</small>
-                </div>
-                <p className="right"><small className="text-secondary">{time}</small></p>
-            </div>
-        </>
     )
 }
 
-function Message(props: any) {
+function MessageDetails(props: any) {
+
+    const subject = props.subject;
+
+    const [messages, setMessages] = useState([] as Message[]);
+
+    useEffect(() => {
+        threadService
+            .getMessages(props.threadId)
+            .then(messages => setMessages(messages));
+    });
+
+
+    return (
+        <div className="detail-view">
+            <h3 className="subject">{subject || "Games!"}</h3>
+
+            <div className="messages">
+                {messages.map(message =>
+                    <MessageView username={message.userId} message={message.body}
+                                 time="11:27"></MessageView>
+                )}
+            </div>
+            {/*<MessageView username="Hans van Os" message="Yo!" time="22:51"></MessageView>*/}
+            {/*<MessageView username="Youssef Airoude" message="Waddup?" time="22:51"></MessageView>*/}
+            {/*<MessageView username="Mathijs Janz" message="Ewa" time="22:51"></MessageView>*/}
+            {/*<MessageView username="Hans van Os" message="Klaar voor de ps5 gaming?" time="22:51"></MessageView>*/}
+
+            <input type="text" className="send-message" placeholder="What would you like to say?"/>
+        </div>
+    )
+}
+
+export function SubjectListCard(props: any) {
+    const subject: string = props.subject;
+    const usernames: string[] = props.usernames;
+    const time = props.time;
+    const splitNames = usernames.map(name => name.split(" "));
+
+    function showChatDetails() {
+
+    }
+
+    return (
+        <div className="h-card active" onClick={showChatDetails}>
+            <div className="flex-column center">
+                <p>{subject}</p>
+                <small>
+                    {splitNames.map(splitName => {
+                        const firstName = splitName[0][0];
+                        let lastName = "";
+                        if (splitName.length > 1) {
+                            lastName = splitName[splitName.length - 1][0]
+                        }
+                        return firstName + lastName;
+                    }).join(", ")
+                    }
+                </small>
+            </div>
+            <p className="right"><small className="text-secondary">{time}</small></p>
+        </div>
+    )
+}
+
+function MessageView(props: any) {
     const username: string = props.username;
     const message: string = props.message;
     const time = props.time;
     return (
         <div className="text-bubble">
-            <div className="profile">
-                {username.split(" ").map(part => part[0])}
-            </div>
+            <div></div>
             <div className="details-container">
                 <div className="sender-details">
                     <div>{username}</div>
@@ -68,10 +108,22 @@ function Message(props: any) {
 }
 
 function MainView() {
+
+    const [threads, setThreads] = useState([] as Thread[]);
+
+    // useEffect(() => {
+    //     threadService
+    //         .getThreads()
+    //         .then(threads => setThreads(threads));
+    // });
+
     return (
         <div className="content-overview">
-            <SubjectCard starter="Hans van Os" usernames={["Youssef, Mathijs"]}
-                         subject="Wat vinden jullie hiervan?"/>
+            <NewThread/>
+            {threads.map(thread =>
+                <SubjectCard starter={thread.sender_id} usernames={thread.receivers}
+                             subject={thread.subject}/>
+            )}
             <SubjectCard starter="Hans van Os" usernames={["Hans van Os"]} subject="Subject2"/>
             <SubjectCard starter="Hans van Os" usernames={["Hans van Os"]} subject="Subject3"/>
             <SubjectCard starter="Hans van Os" usernames={["Hans van Os"]} subject="Subject4"/>
@@ -97,24 +149,60 @@ function SubjectCard(props: any) {
     const usernames: string[] = props.usernames;
     const starter: string = props.starter;
 
-    function seeDetails() {
+    return (
+        <Link style={{textDecoration: "inherit", color: "inherit"}} to={"/" + subject}>
+            <div className="v-card clickable">
+                <div className="header">
+                    {/*Image placeholder*/}
+                </div>
+                <div className="title">
+                    {subject}
+                </div>
+                <div className="content">
+                    <div className="messages">Replies: 1</div>
+                    <div className="usercount">Users: 1</div>
+                </div>
+                <p className="subtext">
+                    Started by: <u>{starter}</u>
+                </p>
+            </div>
+        </Link>
+    )
+}
 
+function NewThread() {
+
+    const starter: string = "Hans van Os";
+
+    const [showButton, setShowButton] = useState(true);
+
+    const showForm = () => {
+        setShowButton(!showButton);
     }
 
     return (
-        <div className="v-card" onClick={seeDetails}>
-            <div className="header">
-                {/*Image placeholder*/}
-            </div>
-            <div className="title">
-                {subject}
-            </div>
-            <p className="content">
-                {usernames.join(", ")}
-            </p>
-            <p className="subtext">
-                Started by: <u>{starter}</u>
-            </p>
+        <div className="v-card">
+            {
+                showButton &&
+                <button className="new-thread clickable" onClick={showForm}>+</button>
+            }
+            {
+                !showButton &&
+                <>
+                    <div className="">
+                        {/*Image placeholder*/}
+                    </div>
+                    <div className="title">
+
+                    </div>
+                    <div className="content">
+
+                    </div>
+                    <p className="subtext">
+                        {/*Started by: <u>{starter}</u>*/}
+                    </p>
+                </>
+            }
         </div>
     )
 }
---
+++ src/ThreadService.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export function getThreads(): Promise<Thread[]>{
+    return axios.get("http://localhost:8080/api/v1/messages/")
+        .then(response => response.data);
+}
+
+export function createThread(subject: string): void {
+    axios.post("http://localhost:8080/api/v1/messages/");
+}
+
+export function getMessages(threadId: number): Promise<Message[]>{
+    return axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.data);
+}
+
+export function addMessage(message: string, threadId: number): void {
+    axios.post("http://localhost:8080/api/v1/messages/" + threadId);
+}
+
+export type Message =
+    {
+        userId: number
+        body: string
+        title: string
+    }
+
+export type Thread =
+    {
+        sender_id: number
+        subject: string
+        receivers: string[]
+        replies: []
+        timestamp: string
+    }
